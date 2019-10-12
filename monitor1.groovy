afa0b63a80aef3821d15e40798d07a89ca6b96dd
wanjalachristopher <52838763+wanjalachristopher@users.noreply.github.com>
2019-10-12 14:39:54 +0300

    Update monitor1.groovy

--- monitor1.groovy
+++ monitor1.groovy
@@ -1,11 +1,12 @@
-job('Backend 1 Monitor') {
+job('Server Monitor') {
   
     triggers {
-        cron('* */5 * * *')
+        cron('*/5 * * * *')
     }
      steps {
 		 httpRequest('http://192.168.144.129:8181/vending-backend-dev/ping/backend?idVendor=0&codUser=jenkins'){
 		 httpMode('GET')
+		 validResponseCodes(200:201)
 		   
 		 }
 	 
