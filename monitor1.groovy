c471ced044de7d5acb75392c43f0de69c1a0b338
wanjalachristopher <52838763+wanjalachristopher@users.noreply.github.com>
2019-10-12 13:22:49 +0300

    Add files via upload

---
+++ monitor1.groovy
@@ -0,0 +1,16 @@
+job('Backend 1 Monitor') {
+  
+    triggers {
+        cron('* */5 * * *')
+    }
+     steps {
+	def res = httpRequest('https://httpbin.org/get'){
+	 httpMode('GET')
+       
+	 }
+	 if(res.status = 200){}
+	  println('Status: '+res.status)
+	  println('Response: '+res.content)
+	}
+  
+}
